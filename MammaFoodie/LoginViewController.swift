ce5c14c9c837909ba7f0db7bee4db474d412d092
HaarisMuneer <hom9@cornell.edu>
2017-02-22 10:32:58 -0500

    adds in FBSDK

---
+++ MammaFoodie/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  MammaFoodie
+//
+//  Created by Haaris Muneer on 2/22/17.
+//  Copyright © 2017 MammaFoodieCorp. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FBSDKLoginKit
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let loginButton = FBSDKLoginButton()
+        loginButton.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
