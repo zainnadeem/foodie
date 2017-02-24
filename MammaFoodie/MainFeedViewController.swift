79d2ea9a036ed19654f214c3643398aabc065130
zainnadeem <zn.nadeem@gmail.com>
2017-02-24 16:59:23 -0500

    added userPageView layout including profileVC, mainfeedVC, searchVC

---
+++ MammaFoodie/MainFeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainFeedViewController.swift
+//  MammaFoodie
+//
+//  Created by Zain Nadeem on 2/24/17.
+//  Copyright © 2017 MammaFoodieCorp. All rights reserved.
+//
+
+import UIKit
+
+class MainFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+}
