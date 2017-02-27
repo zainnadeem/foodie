09240de6df71c8f10b8e7ba0a7e58659f904ff5f
HaarisMuneer <hom9@cornell.edu>
2017-02-27 16:23:36 -0500

    basic setup of profile view complete

---
+++ MammaFoodie/ProfileTableHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderView.swift
+//  MammaFoodie
+//
+//  Created by Haaris Muneer on 2/27/17.
+//  Copyright © 2017 MammaFoodieCorp. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
