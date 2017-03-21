a409e872dcdac4bbd6d0e6c0bd253fb1b7363245
HaarisMuneer <hom9@cornell.edu>
2017-03-21 16:04:29 -0400

    fixing merge conflict

--- MammaFoodie/Secrets.swift
+++ MammaFoodie/Secrets.swift
@@ -20,3 +20,9 @@ let stripeSecret = "sk_test_ILoBhJpbX4bmuygd0NQs23V5"
 let stripePublishableKey = "pk_test_GR7oEMC78jWcX3qsXVXlMsuC"
 
 var backendBaseURL = "http://192.168.1.8:3000"
+
+let postmatesCustomerID = "cus_LBpJsI0U0nL-3k"
+
+let postmatesSandboxKey = "4838334d-bfe1-4308-8e75-8a4234182b4c"
+
+let encodedSandboxKey = "Basic NDgzODMzNGQtYmZlMS00MzA4LThlNzUtOGE0MjM0MTgyYjRjOg=="
